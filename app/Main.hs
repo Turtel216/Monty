0597f48e9dddb6cdfc8c4d3a349ac82a51ea1a7e
Turtel216 <papadimitrios216@gmail.com>
2024-11-14 17:02:09 +0200

    Added Repl tool

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,23 @@
 
 module Main where
 
+import Control.Monad.Trans
+import Parser
+import System.Console.Haskeline
+
+process :: String -> IO ()
+process line = do
+  let res = parseToplevel line
+  case res of
+    Left err -> print err
+    Right ex -> mapM_ print ex
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+main = runInputT defaultSettings loop
+  where
+    loop = do
+      minput <- getInputLine ">>> "
+      case minput of
+        Nothing -> outputStrLn "Ni."
+        Just ":q" -> outputStrLn "Ni."
+        Just input -> (liftIO $ process input) >> loop
